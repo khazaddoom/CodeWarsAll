18ac73e5e97ef1ea95223a92551229f87947ca32
khazaddoom <31121986g@gmail.com>
2018-05-07 18:00:29 +0530

    Pangram program

---
+++ src/pangram/PangramChecker.java
@@ -0,0 +1,25 @@
+package pangram;
+
+public class PangramChecker {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(check("The quick brown fox jumps over the lazy dog."));
+		
+		System.out.println(check("You shall not pass!"));
+		
+	}
+	
+	public static boolean check(String sentence) {
+		
+		sentence = sentence.trim();
+		
+		sentence.toCharArray();
+		
+		
+		
+		return true;
+	}
+	
+
+}
